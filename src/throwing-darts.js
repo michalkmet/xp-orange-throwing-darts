b1940355d5464685dcfb5fbbb90d270b8976faf5
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2024-02-01 16:44:18 +0100

    feat: uat2.1 pass

--- src/throwing-darts.js
+++ src/throwing-darts.js
@@ -1,10 +1,13 @@
 function throwingDarts(inputArr) {
-  if ((inputArr[0] > 4 && inputArr[0] < 11) || inputArr[1] < 11) {
-    return 5;
-  } else if (inputArr[0] < 5) {
-    return 110;
+  let result = 0;
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] > 4 && inputArr[i] < 11) {
+      result += 5;
+    } else if (inputArr[i] < 5) {
+      result += 110;
+    }
   }
-  return 0;
+  return result;
 }
 
 module.exports = throwingDarts;
