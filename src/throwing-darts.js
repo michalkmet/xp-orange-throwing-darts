a5897b68ac65298b1f2add60ba01b7af1ae5edaf
Michal Kmet <Michal.Kmet1@ibm.com>
2024-02-02 15:48:17 +0100

    refactor: complexity

--- src/throwing-darts.js
+++ src/throwing-darts.js
@@ -3,16 +3,26 @@ function throwingDarts(inputArr) {
   let result = 0;
   let allBelowFive = true;
   for (const element of inputArr) {
-    if (element > 4 && element < 11) {
-      result += 5;
-      allBelowFive = false;
-    } else if (element < 5) {
-      result += 10;
-    } else {
-      allBelowFive = false;
-    }
+    [result, allBelowFive] = calculatePoints(element, result, allBelowFive);
   }
+  result += addBonus(allBelowFive, inputArr);
+  return result;
+}
 
+function calculatePoints(element, result, allBelowFive) {
+  if (element > 4 && element < 11) {
+    result += 5;
+    allBelowFive = false;
+  } else if (element < 5) {
+    result += 10;
+  } else {
+    allBelowFive = false;
+  }
+  return [result, allBelowFive];
+}
+
+function addBonus(allBelowFive, inputArr) {
+  let result = 0;
   if (allBelowFive === true && inputArr.length != 0) {
     result += 100;
   }
